3683d4549d708b423124ec60a2c64161971452e6
Alexandre Martins <alexandre.martins.001@gmail.com>
2026-08-14 09:42:08 -0300

    Criação do repositório

---
+++ src/main/java/br/com/datum/DatumSrvScoreClienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.datum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatumSrvScoreClienteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatumSrvScoreClienteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/datum/DatumSrvScoreClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.datum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatumSrvScoreClienteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
